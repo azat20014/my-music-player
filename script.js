fce6ee7fbf530371e3adb503dd31e0ff073d9f0a
azat20014 <azat.garifullin.2001@yandex.ru>
2024-06-15 10:30:41 +0500

    up

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-const CLIENT_ID = '828364428307-rnf73fk6d5e0mp0jvaujq9qept24d8os.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyBj0lXeituQLVJckpefExmacBcraO4h3A8';
-const FOLDER_ID = '10qHaHoxXMrRN1pkPXLkzaGxU2xLlzEpQ';
+document.addEventListener('DOMContentLoaded', function () {
+    const folderId = 'YOUR_FOLDER_ID_HERE';
+    const apiKey = 'YOUR_API_KEY_HERE';
+    const audio = new Audio();
+    let trackList = [];
+    let currentTrackIndex = 0;
 
-const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-const SCOPES = 'https://www.googleapis.com/auth/drive.readonly';
+    // Получаем список файлов из Google Drive
+    fetch(`https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            trackList = data.files.filter(file => file.mimeType === 'audio/mpeg');
+            if (trackList.length > 0) {
+                loadTrack(currentTrackIndex);
+            } else {
+                alert('Нет доступных аудиофайлов.');
+            }
+        });
 
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
+    function loadTrack(index) {
+        if (index >= 0 && index < trackList.length) {
+            const track = trackList[index];
+            audio.src = `https://www.googleapis.com/drive/v3/files/${track.id}?alt=media&key=${apiKey}`;
+            document.getElementById('track-title').textContent = track.name;
+        }
+    }
 
-function initClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-    }).then(() => {
-        gapi.auth2.getAuthInstance().signIn().then(loadFiles);
-    }, (error) => {
-        console.error(JSON.stringify(error, null, 2));
-    });
-}
-
-function loadFiles() {
-    gapi.client.drive.files.list({
-        'q': `'${FOLDER_ID}' in parents and mimeType='audio/mpeg'`,
-        'pageSize': 10,
-        'fields': "nextPageToken, files(id, name)"
-    }).then((response) => {
-        const files = response.result.files;
-        if (files && files.length > 0) {
-            const fileList = document.getElementById('file-list');
-            fileList.innerHTML = '';
-            files.forEach((file) => {
-                const div = document.createElement('div');
-                div.className = 'file-item';
-                div.textContent = file.name;
-                div.onclick = () => playFile(file.id);
-                fileList.appendChild(div);
-            });
+    document.getElementById('play').addEventListener('click', function () {
+        if (audio.paused) {
+            audio.play();
         } else {
-            console.log('No files found.');
+            audio.pause();
         }
     });
-}
 
-function playFile(fileId) {
-    const audioPlayer = document.getElementById('audio-player');
-    audioPlayer.src = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${API_KEY}`;
-    audioPlayer.play();
-}
+    document.getElementById('prev').addEventListener('click', function () {
+        currentTrackIndex = (currentTrackIndex - 1 + trackList.length) % trackList.length;
+        loadTrack(currentTrackIndex);
+        audio.play();
+    });
 
-document.body.onload = handleClientLoad;
+    document.getElementById('next').addEventListener('click', function () {
+        currentTrackIndex = (currentTrackIndex + 1) % trackList.length;
+        loadTrack(currentTrackIndex);
+        audio.play();
+    });
+
+    audio.addEventListener('ended', function () {
+        currentTrackIndex = (currentTrackIndex + 1) % trackList.length;
+        loadTrack(currentTrackIndex);
+        audio.play();
+    });
+});
